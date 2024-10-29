bbda19bd884e00b24714dc1d706f15c357859310
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2024-10-28 22:30:31 -0400

    fixed on mobile

--- app/components/grid.tsx
+++ app/components/grid.tsx
@@ -25,16 +25,16 @@ const areaProjects = [
 ];
 export default function GridComponent(){
     const [area, setArea] = useState(1);
-    const areas = ["Graphics Programming", "Full Stack", "Game Development", "AI/ML"];
+    const areas = ["Graphics", "Full Stack", "Gamedev", "AI/ML"];
     
     return(
         <div>
             <LabelItem label={areas[area] + " Experience"} />
             <div className="flex justify-center space-x-2">
-                <button onClick={()=>{setArea(1)}} className={`bg-slate-500 p-1 pl-4 pr-4 rounded-2xl text-white hover:scale-110 duration-75 hover:bg-slate-400 hover:text-white ${area==1? 'bg-slate-200 text-slate-700 border' : 'bg-slate-500 text-white'}`}>{areas[1]}</button>
-                <button onClick={()=>{setArea(0)}} className={`bg-slate-500 p-1 pl-4 pr-4 rounded-2xl text-white hover:scale-110 duration-75 hover:bg-slate-400 hover:text-white ${area==0? 'bg-slate-200 text-slate-700 border' : 'bg-slate-500 text-white'}`}>{areas[0]}</button>
-                <button onClick={()=>{setArea(2)}} className={`bg-slate-500 p-1 pl-4 pr-4 rounded-2xl text-white hover:scale-110 duration-75 hover:bg-slate-400 hover:text-white ${area==2? 'bg-slate-200 text-slate-700 border' : 'bg-slate-500 text-white'}`}>{areas[2]}</button>
-                <button onClick={()=>{setArea(3)}} className={`bg-slate-500 p-1 pl-4 pr-4 rounded-2xl text-white hover:scale-110 duration-75 hover:bg-slate-400 hover:text-white ${area==3? 'bg-slate-200 text-slate-700 border' : 'bg-slate-500 text-white'}`}>{areas[3]}</button>
+                <button onClick={()=>{setArea(1)}} className={`p-1 pl-2 pr-2 md:pl-4 md:pr-4 rounded-2xl hover:scale-110 duration-75 hover:bg-slate-400 hover:text-white ${area==1? 'bg-slate-200 text-slate-700 border' : 'bg-slate-500 text-white'}`}>{areas[1]}</button>
+                <button onClick={()=>{setArea(0)}} className={`p-1 pl-2 pr-2 md:pl-4 md:pr-4 rounded-2xl hover:scale-110 duration-75 hover:bg-slate-400 hover:text-white ${area==0? 'bg-slate-200 text-slate-700 border' : 'bg-slate-500 text-white'}`}>{areas[0]}</button>
+                <button onClick={()=>{setArea(2)}} className={`p-1 pl-2 pr-2 md:pl-4 md:pr-4 rounded-2xl hover:scale-110 duration-75 hover:bg-slate-400 hover:text-white ${area==2? 'bg-slate-200 text-slate-700 border' : 'bg-slate-500 text-white'}`}>{areas[2]}</button>
+                <button onClick={()=>{setArea(3)}} className={`p-1 pl-2 pr-2 md:pl-4 md:pr-4 rounded-2xl hover:scale-110 duration-75 hover:bg-slate-400 hover:text-white ${area==3? 'bg-slate-200 text-slate-700 border' : 'bg-slate-500 text-white'}`}>{areas[3]}</button>
             </div>
             <div className="flex justify-center ml-5 mr-5">
                 {areaProjects[area]}
--- app/components/intro-card.tsx
+++ app/components/intro-card.tsx
@@ -19,6 +19,7 @@ export default function IntroCard(){
         />
         <div>
           <Card />
+          
           <Footer />
         </div>
       </div>
