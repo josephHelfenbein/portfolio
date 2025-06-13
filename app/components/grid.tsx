0cf6d52741fa20fc35362b1bb81bb79db1d56608
Joseph J Helfenbein <josephhelfenbein@gmail.com>
2025-06-13 17:32:32 -0400

    add gustgrid

--- app/components/grid.tsx
+++ app/components/grid.tsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from "react";
 import LabelItem from "./label-item";
-import { TravelAmuletProjectCard, GameRecommendCard, PhysicsProjectCard, HorrorGameProjectCard, FightingGameProjectCard, ScienceProjectCard, MaterialProjectCard, MendPathProjectCard, PrototypeGameCard, SpiritBoxGameCard, HelpSignalProjectCard, CerebrixProjectCard, ChamaProjectCard, PairGridProjectCard, PatSafeProjectCard, GuardianAngelProjectCard, BlazeGuardProjectCard, COFAProjectCard, FocuraProjectCard, RecapGridProjectCard } from "./project-cards";
+import { TravelAmuletProjectCard, GameRecommendCard, PhysicsProjectCard, HorrorGameProjectCard, FightingGameProjectCard, ScienceProjectCard, MaterialProjectCard, MendPathProjectCard, PrototypeGameCard, SpiritBoxGameCard, HelpSignalProjectCard, CerebrixProjectCard, ChamaProjectCard, PairGridProjectCard, PatSafeProjectCard, GuardianAngelProjectCard, BlazeGuardProjectCard, COFAProjectCard, FocuraProjectCard, RecapGridProjectCard, GustGridProjectCard } from "./project-cards";
 import { motion } from "motion/react";
 const areaProjects = [
     (<div className="grid sm:grid-cols-2 max-w-7xl gap-1 mb-16">
+        <GustGridProjectCard />
         <MaterialProjectCard />
         <PhysicsProjectCard />
     </div>),
--- app/components/project-cards.jsx
+++ app/components/project-cards.jsx
@@ -79,6 +79,43 @@ export function MaterialProjectCard(){
     </motion.div>
   )
 }
+export function GustGridProjectCard(){
+  return (
+   <motion.div initial={{ y: 10, opacity: 0 }}
+    whileInView={{ y: 0, opacity: 1 }}
+    transition={{ default: { type: "spring" },
+    opacity: { ease: "linear" }, duration: 0.15 }} className="flex justify-center backdrop-blur-lg m-0 mt-4 md:m-4 p-4 shadow-lg rounded-xl h-100 md:h-96">
+    <article className="w-4xl">
+    <div className="flex items-start justify-center gap-2">
+    <img src="./gustgrid-logo.svg" width={32} />
+    <h3 className="text-xl font-semibold tracking-tighter">GustGrid</h3>
+    </div>
+    <div>
+      <div className="flex flex-wrap items-start justify-start gap-1 pt-2 pb-3">
+          <img className="h-5 sm:h-6" src="https://img.shields.io/badge/c++-00599C?logo=cplusplus&style=for-the-badge&logoColor=white" />
+          <img className="h-5 sm:h-6" src="https://img.shields.io/badge/opengl-5586A4?logo=opengl&style=for-the-badge&logoColor=white" />
+          <img className="h-5 sm:h-6" src="https://img.shields.io/badge/cuda-76B900?logo=nvidia&style=for-the-badge&logoColor=white" />
+          </div>
+          <h5 className="text-sm tracking-tighter text-center font-semibold">June 2025</h5>
+          <div className="flex gap-0.5 justify-center">
+          <a target="_blank" className="cardLink flex gap-0.5" href="https://github.com/josephHelfenbein/gustgrid">
+          <svg width="20" height="20" viewBox="0 0 30 30" fill="black" className="dark:fill-white" xmlns="http://www.w3.org/2000/svg">
+            <path d="M15,3C8.373,3,3,8.373,3,15c0,5.623,3.872,10.328,9.092,11.63C12.036,26.468,12,26.28,12,26.047v-2.051 c-0.487,0-1.303,0-1.508,0c-0.821,0-1.551-0.353-1.905-1.009c-0.393-0.729-0.461-1.844-1.435-2.526 c-0.289-0.227-0.069-0.486,0.264-0.451c0.615,0.174,1.125,0.596,1.605,1.222c0.478,0.627,0.703,0.769,1.596,0.769 c0.433,0,1.081-0.025,1.691-0.121c0.328-0.833,0.895-1.6,1.588-1.962c-3.996-0.411-5.903-2.399-5.903-5.098 c0-1.162,0.495-2.286,1.336-3.233C9.053,10.647,8.706,8.73,9.435,8c1.798,0,2.885,1.166,3.146,1.481C13.477,9.174,14.461,9,15.495,9 c1.036,0,2.024,0.174,2.922,0.483C18.675,9.17,19.763,8,21.565,8c0.732,0.731,0.381,2.656,0.102,3.594 c0.836,0.945,1.328,2.066,1.328,3.226c0,2.697-1.904,4.684-5.894,5.097C18.199,20.49,19,22.1,19,23.313v2.734 c0,0.104-0.023,0.179-0.035,0.268C23.641,24.676,27,20.236,27,15C27,8.373,21.627,3,15,3z"></path>
+          </svg>
+          <h5 className="text-sm tracking-tighter">
+          GitHub</h5>
+          </a> 
+          </div>
+        </div>
+      <div className="block h-full rounded-lg">
+          <div className="p-3 text-xs/5 font-medium text-gray-900 dark:text-gray-100">
+            <p>A real-time visualization tool of PC airflow and thermal dynamics built in C++, OpenGL, and NVIDIA CUDA. It accurately models heat dissipation, fan performance, and airflow patterns within complex computer chassis geometries, and has a graphical interface to adjust component layouts, enabling rapid design iterations.</p>
+          </div>
+      </div>
+    </article>
+    </motion.div>
+  )
+}
 export function GameRecommendCard(){
   return (
     <motion.div initial={{ y: 10, opacity: 0 }}
